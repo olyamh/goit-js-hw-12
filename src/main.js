d57097c5c78946c59dedecd48690f874cd8db38a
Olyamh <olyamh234@yahoo.com>
2024-12-15 12:04:08 +0200

    changed files

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/css/simple-lightbox.min.css';
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/css/simple-lightbox.min.css';
 
 import { createMarkup } from './js/render-functions';
 import { needPhotos } from './js/pixabay-api';
